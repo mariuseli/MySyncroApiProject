21e3c07bef33093872b2462fe01c85a24cd3f873
mariuseli <mariuseli@users.noreply.github.com>
2020-07-20 12:13:24 +0200

    .gitignore is now working

--- MySyncroAPI.Business/MyContacts/Commands/CreateContactCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace MySyncroAPI.Business.MyContacts.Commands
-{
-    public class CreateContactCommand : IRequest<int>
-    {
-        public ContactDto ContactToCreate { get; set; }
-    }
-}
